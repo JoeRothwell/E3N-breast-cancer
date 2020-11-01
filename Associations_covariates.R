1559fa0afdc31b9b40ec66f2901234ea84ea2222
Joe <joerothwell1@gmail.com>
2020-11-01 22:34:38 +0100

    Updated and improved plot

--- Associations_covariates.R
+++ Associations_covariates.R
@@ -1,7 +1,7 @@
 # Association BC risk factors - metabolites
 library(tidyverse)
 meta <- read_csv("metadata.csv", na = "9999")
-ctrl <- meta$CT == 0 & meta$MENOPAUSE == 1
+ctrl <- meta$CT == 0 #& meta$MENOPAUSE == 1
 
 # Get control subjects and remove outliers from metabolite data
 ints0 <- read_tsv("1510_XMetaboliteE3N_cpmg_unscaled.txt")[ctrl, ]
@@ -11,25 +11,27 @@ ints0[logicalmat] <- NA
 
 # Scale to unit variance
 ints <- scale(ints0)
-meta1 <- meta %>% filter(CT == 0 & MENOPAUSE == 1) %>% select(FASTING, SMK, ALCOHOL, Life_Alcohol_Pattern_1, 
+meta1 <- meta %>% filter(CT == 0) %>% select(FASTING, SMK, ALCOHOL, Life_Alcohol_Pattern_1, 
       BMI, AGE, BP, TTAILLE, Trait_Horm, MENOPAUSE, DIABETE, CO, DURTHSBMB, CENTTIME, STOCKTIME,
       WEEKS)
 
 # Risk factors to be tested
 #FASTING, SMK, BMI, AGE, BP, TTAILLE, Trait_Horm, MENOPAUSE, DIABETE, CO
 library(broom)
-lm.fasting <- function(x) lm(FASTING ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.alc     <- function(x) lm(ALCOHOL ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.lifealc <- function(x) lm(Life_Alcohol_Pattern_1 ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.smk     <- function(x) lm(SMK ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.bmi     <- function(x) lm(BMI ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.age     <- function(x) lm(AGE ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.bp      <- function(x) lm(BP ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.ttaille <- function(x) lm(TTAILLE ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.horm    <- function(x) lm(Trait_Horm ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.diabet  <- function(x) lm(DIABETE ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
-lm.co      <- function(x) lm(CO ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.meno    <- function(x) lm(MENOPAUSE ~ x + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.fasting <- function(x) lm(FASTING ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.alc     <- function(x) lm(ALCOHOL ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.lifealc <- function(x) lm(Life_Alcohol_Pattern_1 ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.smk     <- function(x) lm(SMK ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.bmi     <- function(x) lm(BMI ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.age     <- function(x) lm(AGE ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.bp      <- function(x) lm(BP ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.ttaille <- function(x) lm(TTAILLE ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.horm    <- function(x) lm(Trait_Horm ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.diabet  <- function(x) lm(DIABETE ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
+lm.co      <- function(x) lm(CO ~ x + MENOPAUSE + DURTHSBMB + CENTTIME + STOCKTIME, data = meta1)
 
+fits0 <- apply(ints, 2, lm.meno) %>% map_df(tidy)
 fits1 <- apply(ints, 2, lm.fasting) %>% map_df(tidy) 
 fits1a <- apply(ints, 2, lm.alc) %>% map_df(tidy) 
 fits1b <- apply(ints, 2, lm.lifealc) %>% map_df(tidy) 
@@ -42,27 +44,35 @@ fits7 <- apply(ints, 2, lm.horm) %>% map_df(tidy)
 fits9 <- apply(ints, 2, lm.diabet) %>% map_df(tidy) 
 fits10 <- apply(ints, 2, lm.co) %>% map_df(tidy)
 
-all <- bind_rows(fits1, fits1a, fits1b, fits2, fits3, fits4, fits5, fits6, fits7,
-                 fits9, fits10) %>% filter(str_detect(term, "x")) 
+all <- bind_rows(fits0, fits1, fits1a, fits1b, fits2, fits3, fits4, fits5, fits6, fits7,
+                 fits9, fits10) %>% filter(str_detect(term, "x"))  %>%
+  mutate(p.adj = p.adjust(p.value, method = "fdr")) %>%
+  bind_cols(cmpd = rep(colnames(ints), 12))
 
 library(RColorBrewer)
 # Different colour palettes
-set2 <- rep(brewer.pal(8, "Set2"), each = 43, length.out=nrow(all))
-#accent <- rep(brewer.pal(8, "Accent") , each = 43, length.out=nrow(all))
-#rain <- rep(rainbow(12), each = 43, length.out = nrow(all))
+set2 <- rep(brewer.pal(8, "Set2"), each = 43, length.out = nrow(all))
+accent <- rep(brewer.pal(8, "Accent") , each = 43, length.out = nrow(all))
+rain <- rep(rainbow(12), each = 43, length.out = nrow(all))
 
 # Plot (add col palette as necessary)
-plot(-log10(all$p.value), col = set2, pch = 19, cex = 0.6)
+#plot(-log10(all$p.value), col = set2, pch = 19, cex = 0.6)
 
 # x axis width
 x = 1:nrow(all)
 
 # draw empty plot
-plot(NULL, xlim=c(0, nrow(all)), ylim=c(0, max(-log10(all$p.value))), xaxt='n', 
+plot(NULL, xlim=c(0, nrow(all)), ylim=c(0, max(-log10(all$p.value))), xaxt='n',
      ylab='-log10(p-value)', xlab='')
 points(x, -log10(all$p.value), pch=19, col=set2, cex = 0.6)
 
 # axis labels
-labs <- c("Fasting", "Alcohol", "Life alc.", "Smoke", "BMI", "Age", "BP", 
-          "WC", "HRT", "Diabetes", "Contracep.")
-axis(1, at = c(21, 64, 107, 150, 193, 236, 279, 322, 365, 408, 451), labels = labs, las=3)
+labs <- c("Meno", "Fast", "Alc", "Life alc.", "Smoke", "BMI", "Age", "BP", 
+          "WC", "HRT", "Diabet.", "OC")
+abline(h = -log(0.05), col = "grey")
+abline(h = -log(0.016), col = "red")
+axis(1, at = c(21, 64, 107, 150, 193, 236, 279, 322, 365, 408, 451, 494), labels = labs, 
+     las=1, cex.axis = 0.7)
+text(350, 22.5, "Choline", pos = 2, cex = 0.7)
+text(360, 21.5, "Glycerophosphocholine", pos = 2, cex = 0.7)
+
